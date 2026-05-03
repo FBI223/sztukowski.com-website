3c023edbeb1e1dcfc166f3127a3cc147c1a32af5
FBI223 <sztukowski@vp.pl>
2026-05-03 16:58:12 +0200

    fix path in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  // Ustaw site jeśli deploymasz na GitHub Pages lub własną domenę:
-  // site: 'https://marcin.sztukowski.com',
+    output: 'static',
+    site: 'https://FBI223.github.io',
+    base: '/sztukowski.com-website',
 });
